56b28ed39183fa9ae5521920d7d32c9a252adde9
René Kijewski <rene.kijewski@fu-berlin.de>
2022-04-21 09:52:14 +0200

    Add simple test for completeness

---
+++ tests/simple.rs
@@ -0,0 +1,18 @@
+use askama_enum::EnumTemplate;
+
+#[derive(EnumTemplate)]
+enum MyEnum {
+    #[template(ext = "txt", source = "A")]
+    A,
+    #[template(ext = "txt", source = "B")]
+    B,
+    #[template(ext = "txt", source = "C")]
+    C,
+}
+
+#[test]
+fn test() {
+    assert_eq!(MyEnum::A.to_string(), "A");
+    assert_eq!(MyEnum::B.to_string(), "B");
+    assert_eq!(MyEnum::C.to_string(), "C");
+}
